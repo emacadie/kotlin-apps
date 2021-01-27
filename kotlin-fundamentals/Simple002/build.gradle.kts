2852b2df50f133e3bc49f8adbe84bece41aa551c
Eric MacAdie <eric@MacAdie.net>
2021-01-27 00:14:01 -0600

    Working w/commands

--- kotlin-fundamentals/Simple002/build.gradle.kts
+++ kotlin-fundamentals/Simple002/build.gradle.kts
@@ -39,45 +39,26 @@ tasks.run {
 }
 
 application {
-    // mainClassName = "cli.app.AppKt"
-    // mainClass.set( "info.shelfunit.tutorial.AppRunner" )
+    // we could set main class here:
     // mainClass.set( "info.shelfunit.tutorial.module02.PersonRunnerKt" )
-    // mainClass.set( "info.shelfunit.tutorial.module03.Module03RunnerKt" )
-    // mainClass.set( "info.shelfunit.tutorial.module04.Module04RunnerKt" )
-    // mainClass.set( "info.shelfunit.tutorial.module05.Module05RunnerKt" )
-    // mainClass.set( "info.shelfunit.tutorial.module06.Module06RunnerKt" )
-    // Program has a main function in a companion object
-    // its main fun is annotated w/@JvmStatic and must be called as "Program" NOT "ProgramKt"
-    // mainClass.set( "info.shelfunit.tutorial.module06.Program" )
-    // mainClass.set( "info.shelfunit.tutorial.module07.Module07RunnerKt" )
-    // mainClass.set( "info.shelfunit.tutorial.module08.Module08RunnerKt" )
-    // mainClass.set( "info.shelfunit.tutorial.module09.Module09RunnerKt" )
-    // mainClass.set( "info.shelfunit.tutorial.module10.Module10RunnerKt" )
-    // mainClass.set( "info.shelfunit.tutorial.module11.Module11RunnerKt" )
-    // has null pointer exception
-    // mainClass.set( "info.shelfunit.tutorial.module11.Organizer" )
-    // mainClass.set( "info.shelfunit.tutorial.module12.Module12RunnerKt" )
-    // mainClass.set( "info.shelfunit.tutorial.module13.Module13RunnerKt" )
 
     if ( project.hasProperty( "mainArgs" ) ) {
         println( "We have mainArgs" )
         println( project.findProperty( "mainArgs" ) )
-        // project.set
-    }
-    if ( project.hasProperty( "ekmMain" ) ) {
-        println( "We have ekmMain" )
-        // println( project.findProperty( "mainArgs" ) )
-        mainClass.set( project.findProperty( "ekmMain" ).toString() )
-        // project.set
-    }
-    if ( project.hasProperty( "mainClass" ) ) {
-        println( "We have mainClass" )
-        // println( project.findProperty( "mainArgs" ) )
-        mainClass.set( project.findProperty( "mainClass" ).toString() )
-        // project.set
     }
 
+    // or pass it in on command line
+    if ( project.hasProperty( "mainClass" ) ) {
+        println( "We have mainClass" )
+        mainClass.set( project.findProperty( "mainClass" ).toString() )
+    }
+    // we can pass it in on command line w/another name to reduce ambiguity
+    if ( project.hasProperty( "ekmMain" ) ) {
+        println( "We have ekmMain" )
+        mainClass.set( project.findProperty( "ekmMain" ).toString() )
+    }
 }
+
 val compileKotlin: KotlinCompile by tasks
 compileKotlin.kotlinOptions {
     jvmTarget = "11"
